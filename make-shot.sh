ec4b20dd5471ee20d5dd6d2e140225ad607550dc
MetroWind <chris.corsair@gmail.com>
2019-08-06 14:36:56 -0700

    Better screenshot...

--- make-shot.sh
+++ make-shot.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
-convert -size 877x576 xc:black -fill white -draw "path 'M 100,0 C 100,400 777,376 777,576 V 1000 H 1000 V -10 H 100 L 100,0'" mask.png
+Curve="M 100,0 C 100,400 777,376 777,576"
+magick -size 877x576 xc:black -fill white -draw "path '${Curve} V 1000 H 1000 V -10 H 100 L 100,0'" mask.png
+magick -size 877x576 xc:transparent -fill none -strokewidth 3 -draw "stroke white path '${Curve}'" mask-line.png
 composite dark.png light.png mask.png test.png
-convert test.png \( light.png -channel a -separate +channel \) -alpha off -compose CopyOpacity -composite shot.png
-rm test.png mask.png
+magick test.png \( light.png -channel a -separate +channel \) -alpha off -compose CopyOpacity -composite mask-line.png -channel A -compose Dst_Out -composite shot.png
+rm test.png mask.png mask-line.png
